e6d7a27c4b78488e90393efdc5052f34a9e58440
Aaron <aaronwebster88@gmail.com>
2019-04-29 13:39:58 -0400

    photogram-server initial commit

--- controllers/photocontroller.js
+++ controllers/photocontroller.js
@@ -1,19 +1,29 @@
 const router = require('express').Router();
 const Photo = require('../db').import('../models/photo');
 const multer = require('multer');
-const path = require('path');
+const multerS3 = require('multer-s3');
+const AWS = require('aws-sdk');
 const validateSession = require('../middleware/validate-session');
 
-let storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './uploads/')
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname)
-    }
+let s3 = new AWS.S3({
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    region: process.env.REGION
 });
 
-let upload = multer({ storage: storage, fileSize: 100000000 });
+let upload = multer({ 
+    storage: multerS3({
+        s3: s3,
+        bucket: 'photogram-bucket',
+        metadata: function (req, file, cb) {
+            cb(null, { fieldName: file.fieldname });
+        },
+        key: function (req, file, cb) {
+            cb(null, Date.now() + '-' + file.originalname)
+        }
+    }), 
+    // fileSize: 100000000 
+});
 
 // router.get('/', (req, res) => { //TESTING OVERALL ENDPOINT
 //     res.json({ message: "TEST IS GOOOOOOOOOD" })
@@ -48,7 +58,7 @@ router.get('/:id', (req, res) => { // GET for serving photos by ID
         where: { id: req.params.id, }
     })
         .then(foundPhoto => {
-            res.sendFile(path.resolve('./' + foundPhoto.path))
+            res.status(200).json(foundPhoto);
         })
         .catch(err => {
             res.status(500).json(err);
@@ -56,9 +66,10 @@ router.get('/:id', (req, res) => { // GET for serving photos by ID
 });
 
 router.post('/upload', validateSession, upload.single('photo'), (req, res) => { //POST user uploads a single photo
+    console.log('inside POST method');
     options = { multi: true };
     Photo.create({
-        path: req.file.path,
+        path: req.file.location,
         user_id: req.user.id,
         caption: req.body.caption,
         username: req.user.username
@@ -69,7 +80,7 @@ router.post('/upload', validateSession, upload.single('photo'), (req, res) => { 
 
 router.put('/:id', validateSession, upload.single('photo'), (req, res) => { //PUT user updates a single photo
     Photo.update({
-        path: req.file.path,
+        path: req.file.location,
         caption: req.body.caption
     },
         {
